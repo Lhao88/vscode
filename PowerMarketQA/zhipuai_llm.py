6b37fb995ff6ae7991c4380228450b19719ac191
lhao <lhao1346@163.com>
2024-11-09 20:10:22 +0800

    1109

--- PowerMarketQA/zhipuai_llm.py
+++ PowerMarketQA/zhipuai_llm.py
@@ -1,66 +1,23 @@
-#!/usr/bin/env python
-# -*- encoding: utf-8 -*-
+import zhipuai
+from typing import Any
+from llama_index.core.llms import CustomLLM,LLMMetadata,CompletionResponse
+from llama_index.core.llms.callbacks import llm_completion_callback
+class ZhipuLLM(CustomLLM):
+    model_name: str = "chatglm_turbo"
+    context_window: int = 3900
+    num_output: int = 256
 
-from typing import Any, List, Mapping, Optional, Dict
-from langchain_core.callbacks.manager import CallbackManagerForLLMRun
-from langchain_core.language_models.llms import LLM
-from zhipuai import ZhipuAI
-
-import os
-
-# 继承自 langchain.llms.base.LLM
-class ZhipuAILLM(LLM):
-    # 默认选用 glm-4
-    model: str = "glm-4"
-    # 温度系数
-    temperature: float = 0.1
-    # API_Key
-    api_key: str = None
+    @property
+    def metadata(self) -> LLMMetadata:
+        """Get LLM metadata."""
+        return LLMMetadata(
+            context_window=self.context_window,
+            num_output=self.num_output,
+            model_name=self.model_name,
+        )
     
-    def _call(self, prompt : str, stop: Optional[List[str]] = None,
-                run_manager: Optional[CallbackManagerForLLMRun] = None,
-                **kwargs: Any):
-        client = ZhipuAI(
-            api_key = self.api_key
-        )
 
-        def gen_glm_params(prompt):
-            '''
-            构造 GLM 模型请求参数 messages
-
-            请求参数：
-                prompt: 对应的用户提示词
-            '''
-            messages = [{"role": "user", "content": prompt}]
-            return messages
-        
-        messages = gen_glm_params(prompt)
-        response = client.chat.completions.create(
-            model = self.model,
-            messages = messages,
-            temperature = self.temperature
-        )
-
-        if len(response.choices) > 0:
-            return response.choices[0].message.content
-        return "generate answer error"
-
-
-    # 首先定义一个返回默认参数的方法
-    @property
-    def _default_params(self) -> Dict[str, Any]:
-        """获取调用API的默认参数。"""
-        normal_params = {
-            "temperature": self.temperature,
-            }
-        # print(type(self.model_kwargs))
-        return {**normal_params}
-
-    @property
-    def _llm_type(self) -> str:
-        return "Zhipu"
-
-    @property
-    def _identifying_params(self) -> Mapping[str, Any]:
-        """Get the identifying parameters."""
-        return {**{"model": self.model}, **self._default_params}
+    @llm_completion_callback()
+    def complete(self, prompt: str, **kwargs: Any) -> CompletionResponse:
+        response = invoke_prompt(prompt)
+        return CompletionResponse(text=response)
